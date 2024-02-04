6f53c7a1192c98a2fe988cfbf021cfaa465b6265
Janarthanan-97 <janamadhav13@gmail.com>
2024-02-04 14:30:41 +0530

    01

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -10,6 +10,7 @@ function About() {
             <h2>About <span>me</span></h2>
             <h4>Full stack developer</h4>
             <p>Welcome to my portfolio! I'm a dedicated and skilled Full Stack Developer with a passion for creating elegant and efficient web solutions. With a keen eye for detail and a deep understanding of both front-end and back-end technologies, I bring ideas to life through beautifully designed, user-friendly, and high-performing applications. </p>
+            <a className='btn-box' href='https://www.dropbox.com/scl/fi/czp17gkkxycdmca4rvpnf/JANARTHANAN.pdf?rlkey=isghu6gruckzphk2n5noyvbnc&dl=0&raw=1' target='_blank'>Resume</a>
         </div>
     </section>
   )
