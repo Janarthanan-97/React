0027297f8e6a6d3c1f4891fdaa1bf6b2e3b9ff5d
Janarthanan-97 <janamadhav13@gmail.com>
2024-03-16 15:04:05 +0530

    02

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -4,10 +4,17 @@ import './Contact.css'
 function Contact() {
   return (
     <div className='contact-container'>
-      <h4>Contact</h4>
-      <div>
-        <div></div>
+      <h4>CONTACT</h4>
+      <div className='contact-div'>
+        <div className='contact-left'>
+          <a href='Tel:9876543210'><i class="fa-solid fa-phone-volume"></i><span>9876543210</span></a>
+          <a href='mailto:email@gmail.com'><i class="fa-solid fa-envelope"></i><span>email@gmail.com</span></a>
+        </div>
         <div className='center-line'></div>
+        <div className='contact-right'>
+          <textarea name="" id="" cols="30" rows="10" placeholder='Write yor message here!!'></textarea>
+          <button>Submit</button>
+        </div>
       </div>
     </div>
   )
