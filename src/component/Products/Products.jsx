64f2f84af5b630c18dd493910398ba1575ba7bcd
Janarthanan-97 <janamadhav13@gmail.com>
2024-03-21 22:27:35 +0530

    05

--- src/component/Products/Products.jsx
+++ src/component/Products/Products.jsx
@@ -11,13 +11,13 @@ function Products() {
         <div className='product-nav'>
           <Link to='/'>Home</Link> <span>/</span> <Link to='/products' >Products</Link>
         </div>
-        <div className='product-image-container'>
+        {/* <div className='product-image-container'>
           <Link to='/products/roasted-peanuts'><ProductCard url='https://www.dropbox.com/scl/fi/ity51b7i74vo9v07u0h13/roasted-peanut.jpg?rlkey=31opxd0ejkdj28gkonsdhz4tb&dl=0&raw=1' /></Link>
           <Link to='/products/raw-peanuts'><ProductCard /></Link>
           <Link to='/products/virgin-groundnut-oil'><ProductCard url='https://www.dropbox.com/scl/fi/q83tm2ty2ef3eyd255n0a/virgin-groundnut-oil.jpg?rlkey=34kbhv1esl4ly9pj4h7biovkn&dl=0&raw=1' /></Link>
           <Link to='/products/virgin-coconut-oil'><ProductCard url='https://www.dropbox.com/scl/fi/mpewe3dfyz4ly5nvg6xvq/virgin-coconut-oil.jpg?rlkey=236p74f8hbzefr4hj4or9h1d6&dl=0&raw=1' /></Link>
           <Link to='/products/pure-coconut-oil'><ProductCard /></Link>
-        </div>
+        </div> */}
       </div>
     </section>
   )
