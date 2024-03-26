480574b3a131c3c95be2d42b25cd9b52b9070bbd
Janarthanan-97 <janamadhav13@gmail.com>
2024-03-26 23:42:38 +0530

    nav product added

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -7,15 +7,17 @@ function Navbar() {
   const [hideExceptProduct, setHideExceptProduct] = useState('flex')
   const [showProductItems, setShowProductItem] = useState('none')
   const [isChanged, setIsChaged] = useState(0)
-  const handleProductClick= ()=>{
-    setIsChaged(pre=>{
-      if(pre==0){
+  const [navChange, setNavChange] = useState(2)
+  const navArray = ['#1','#2','#3','#4','#5']
+  const handleProductClick = () => {
+    setIsChaged(pre => {
+      if (pre == 0) {
         setProductDropDisplay('flex')
         setHideExceptProduct('none')
         setShowProductItem('flex')
         return 1;
       }
-      else{
+      else {
         setProductDropDisplay('none')
         setHideExceptProduct('flex')
         setShowProductItem('none')
@@ -26,31 +28,56 @@ function Navbar() {
   return (
     <div className='nav-container'>
       <h2>Sri Sai Industries</h2>
-      <div className='nav-link-container' style={{'--hide-except-product':`${hideExceptProduct}`}}>
+      <div className='nav-link-container' style={{ '--hide-except-product': `${hideExceptProduct}` }}>
 
-        <NavLink className='nav-item' to='/' ><span className='icon'><i class="fa-solid fa-house"></i></span><div>Home</div></NavLink>
+        <NavLink className='nav-item' to='/' ><span className='icon'><i className="fa-solid fa-house"></i></span><div>Home</div></NavLink>
 
-        <NavLink className='nav-item' to='/about' ><span className='icon'><i class="fa-solid fa-circle-info"></i></span ><div>About</div></NavLink>
+        <NavLink className='nav-item' to='/about' ><span className='icon'><i className="fa-solid fa-circle-info"></i></span ><div>About</div></NavLink>
 
         {/* /////large screen product */}
-        <NavLink className='nav-item' onClick={handleProductClick} ><span style={{color:"white"}} className='icon'><i class="fa-solid fa-cart-shopping"></i></span><div style={{color:"white"}}>Products</div>
-        <div className='product-drop-down' style={{'--drop':`${productDropDisplay}`}} >
-          <NavLink  to='/products/raw-peanuts' ><span className='icon' ><i class="fa-solid fa-cart-shopping"></i></span><div>Raw peanuts</div></NavLink>
-          <NavLink  to='/products/roasted-peanuts' ><span className='icon'><i class="fa-solid fa-cart-shopping"></i></span><div>Roasted Peanuts</div></NavLink>
-          <NavLink  to='/products/virgin-groundnut-oil' ><span className='icon'><i class="fa-solid fa-cart-shopping"></i></span><div>Groundnut Oil</div></NavLink>
-          <NavLink  to='/products/virgin-coconut-oil' ><span className='icon'><i class="fa-solid fa-cart-shopping"></i></span><div>Coconut oil</div></NavLink>
+        <NavLink className='nav-item' onClick={handleProductClick} ><span style={{ color: "white" }} className='icon'><i className="fa-solid fa-cart-shopping"></i></span><div style={{ color: "white" }}>Products</div>
+          <div className='product-drop-down' style={{ '--drop': `${productDropDisplay}` }} >
+            <NavLink to='/products/raw-peanuts' ><span className='icon' ><i className="fa-solid fa-cart-shopping"></i></span><div>Raw peanuts</div></NavLink>
+            <NavLink to='/products/roasted-peanuts' ><span className='icon'><i className="fa-solid fa-cart-shopping"></i></span><div>Roasted Peanuts</div></NavLink>
+            <NavLink to='/products/virgin-groundnut-oil' ><span className='icon'><i className="fa-solid fa-cart-shopping"></i></span><div>Groundnut Oil</div></NavLink>
+            <NavLink to='/products/virgin-coconut-oil' ><span className='icon'><i className="fa-solid fa-cart-shopping"></i></span><div>Coconut oil</div></NavLink>
           </div>
         </NavLink>
 
         {/* ////mini-screen-product */}
-        <div className='product-nav-items-container' style={{display:`${showProductItems}`}} >
-          <NavLink  to='/products/raw-peanuts' ><span className='icon' ><i class="fa-solid fa-cart-shopping"></i></span><div>Raw peanuts</div></NavLink>
-          <NavLink  to='/products/roasted-peanuts' ><span className='icon'><i class="fa-solid fa-cart-shopping"></i></span><div>Roasted Peanuts</div></NavLink>
-          <NavLink  to='/products/virgin-groundnut-oil' ><span className='icon'><i class="fa-solid fa-cart-shopping"></i></span><div>Groundnut Oil</div></NavLink>
-          <NavLink  to='/products/virgin-coconut-oil' ><span className='icon'><i class="fa-solid fa-cart-shopping"></i></span><div>Coconut oil</div></NavLink>
-          <a ><span className='icon' onClick={handleProductClick}><i class="fa-solid fa-arrow-right-from-bracket"></i></span><div>Back</div></a>
+        {/* <span className='icon' ><i className="fa-solid fa-cart-shopping"></i></span> */}
+        <div className='product-nav-items-container' style={{ display: `${showProductItems}` }} >
+          <div className='nav-arrow nav-arrow-left product-nav-other'>
+            <a href={navArray[navChange]} onClick={()=>{setNavChange(pre=>{
+              if(pre==4){
+                return 2
+              }
+              if(pre>=0){
+                return pre-1
+              }
+            })}} ><i className="fa-solid fa-arrow-left"></i></a>
+          </div>
+          <div className='product-nav-items' >
+            <NavLink to='/products/raw-peanuts' id='1' >Raw peanuts</NavLink>
+            <NavLink to='/products/roasted-peanuts' id='2' >Roasted Peanuts</NavLink>
+            <NavLink to='/products/virgin-groundnut-oil' id='3' >Virgin Groundnut Oil</NavLink>
+            <NavLink to='/products/virgin-coconut-oil' id='4' >Virgin Coconut oil</NavLink>
+            <NavLink to='/products/pure-coconut-oil' id='5' >Pure Coconut oil</NavLink>
+          </div>
+          <div className='nav-arrow nav-arrow-right product-nav-other' >
+            <a href={navArray[navChange]} onClick={()=>{setNavChange(pre=>{
+              if(pre==0){
+                return 3
+              }
+              if(pre<5)
+              {
+                return pre+1
+              }
+            })}}><i className="fa-solid fa-arrow-right"></i></a>
+          </div>
+          <div className='nav-back product-nav-other' ><a><span className='icon' onClick={handleProductClick}><i className="fa-solid fa-bars"></i></span><div>Navigate</div></a></div>
         </div>
-        <NavLink className='nav-item' to='/contact' ><span className='icon'><i class="fa-solid fa-address-card"></i></span><div>Contact</div></NavLink>
+        <NavLink className='nav-item' to='/contact' ><span className='icon'><i className="fa-solid fa-address-card"></i></span><div>Contact</div></NavLink>
       </div>
     </div>
   )
